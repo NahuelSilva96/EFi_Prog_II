7f7986d7ff5b046e1114aac3d2ac412563d05b78
IvanDanielEscobar <i.escobar@itecriocuarto.org.ar>
2025-10-14 00:18:40 -0300

    13/10

--- EFI_Equipo5/WebPeriodistas/src/components/Navbar.jsx
+++ EFI_Equipo5/WebPeriodistas/src/components/Navbar.jsx
@@ -1,14 +1,30 @@
 import { useNavigate } from 'react-router-dom';
-import React from 'react'; 
+import React, { useState, useEffect } from 'react'; 
 import { Button } from 'primereact/button';
 
 
 
 function Navbar() {
   let navigate = useNavigate();
+  const [scrolled, setScrolled] = useState(false);
   
+  useEffect(() => {
+      const handleScroll = () => {
+        if (window.scrollY > 0) { 
+          setScrolled(true);
+        } else {
+          setScrolled(false);
+        }
+      };
+      window.addEventListener('scroll', handleScroll);
+      return () => {
+        window.removeEventListener('scroll', handleScroll);
+      };
+    }, []);
+
+    
       return (
-        <nav className='nav'>
+        <nav className={`nav ${scrolled ? 'scrolled' : ''}`}>
           <section className='nav-left'>
             <article className='logo'>
               <img 

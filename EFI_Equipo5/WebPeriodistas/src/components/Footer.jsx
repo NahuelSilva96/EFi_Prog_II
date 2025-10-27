0ec56214914ca75beb3983bb28ea665df7479fbe
Matias Ibarra <m.ibarra@itecriocuarto.org.ar>
2025-10-26 22:38:23 -0300

    footer

--- EFI_Equipo5/WebPeriodistas/src/components/Footer.jsx
+++ EFI_Equipo5/WebPeriodistas/src/components/Footer.jsx
@@ -44,43 +44,35 @@ function Footer() {
               <img src="./public/img/1.png" alt="tecnored" />
             </a>
           </li>
-          <li><a href="">Super frutas </a></li>
-          <li><a href="">CARNICERIA EMMITA</a></li>
-          <li><a href="">RIOPLAST</a></li>
+          <li>
+            <a href="">
+              <img src="./public/img/RP.jpeg" alt="" />
+            </a>
+          </li>
+          <li>
+            <a href="">
+              <img src="./public/img/CE.png" alt="" />
+            </a>
+          </li>
+          <li>
+            <a href="">
+              <img src="./public/img/SF.png" alt="" />
+            </a>
+          </li>
 
         </ul>
       </section>
 
       <div className="footer-content">
-        {/* Sección de Enlaces */}
-        <ul className="footer-links">
-          <li onClick={() => navigate('/noticias')}>Noticias</li>
-          <li onClick={() => navigate('/contacto')}>Contacto</li>
-          <li onClick={() => navigate('/aviso-legal')}>Aviso Legal</li>
-        </ul>
         
         {/* Redes Sociales  */}
-        <div classname="">
-<<<<<<< Updated upstream
-          <section className="derecha-footer">
-            <article className="redes-container-footer">
-              <div className="social-media-footer">
-                <a target="_blank" href="https://www.instagram.com/equipodeperiodistasr4?igsh=M3VmMXUxeWhvamI0"><i className="pi pi-instagram"></i></a>
-              </div>
-              <div className="social-media-footer">
-                <a target="_blank" href="https://x.com/periodistasrio4"><i className="pi pi-twitter"></i></a>
-              </div>
-              <div className="social-media-footer">
-                <a target="_blank" href="https://www.youtube.com/@periodistasrio4"><i className="pi pi-youtube"></i></a>
-              </div>
-              <div className="social-media-footer">
-                <a target="_blank" href="https://www.tiktok.com/@periodistasrio4"><i className="pi pi-tiktok"></i></a>
-              </div>
-            </article>
-          </section>
-=======
+        <div className="">
             
-            <section className="">
+            <section className="footer-top-section">
+                <article className="logo_footer">
+                  <img src="./public/img/logot.png"/>
+                </article>
+
                 <article className="redes-container-footer">
                   <div className="social-media-footer">
                     <i className="pi pi-instagram"></i>
@@ -96,12 +88,19 @@ function Footer() {
                   </div>
                 </article>
             </section>
->>>>>>> Stashed changes
+
         </div>
 
         {/* Sección de Copyright */}
         <div className="copyright">
           <p>&copy; {new Date().getFullYear()} WebPeriodistas. Todos los derechos reservados.</p>
+          
+          <ul className="footer-links">
+            <li onClick={() => navigate('/noticias')}>Noticias</li>
+            <li onClick={() => navigate('/contacto')}>Contacto</li>
+            <li onClick={() => navigate('/aviso-legal')}>Aviso Legal</li>
+          </ul>
+
         </div>
 
       </div>

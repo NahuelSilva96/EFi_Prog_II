174d70f8bd435ba51d2ec07426d08da8147057b8
IvanDanielEscobar <i.escobar@itecriocuarto.org.ar>
2025-11-05 22:50:24 -0300

    actualizando main

---
+++ WebPeriodistas/WebPeriodistas/src/pages/Plantel.jsx
@@ -0,0 +1,10 @@
+function Plantel() {
+  return (
+    <div>
+      <h1>Plantel</h1>
+      <p>plantel...</p>
+    </div>
+  );
+}
+
+export default Plantel;
---
+++ WebPeriodistas/WebPeriodistas/src/pages/Socios.jsx
@@ -0,0 +1,10 @@
+function Socios() {
+  return (
+    <div>
+      <h1>Socios</h1>
+      <p>Socios...</p>
+    </div>
+  );
+}
+
+export default Socios;
